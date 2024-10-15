7cb98a05763058b6d3555939ef342efa044b10de
rajthanusan <rajthanusan08@gmail.com>
2024-10-15 16:45:44 +0530

    code added

--- src/components/common/navbar.js
+++ src/components/common/navbar.js
@@ -18,11 +18,7 @@ export function Nav() {
         />
       </Link>
       <Link to="/register">
-        <Button
-          label="Register"
-          className="p-button-sm mx-2 custom-darkblue-button"
-          icon="pi pi-user-plus"
-        />
+       
       </Link>
     </>
   );
@@ -35,7 +31,7 @@ export function Navuser() {
   const username = loggedInUser ? JSON.parse(loggedInUser).username : "";
 
   const items = [
-    { label: "Home", icon: "pi pi-home", url: "/Employee" },
+    { url: "/Employee" },
     { label: "Apply Leave", icon: "pi pi-users", url: "/applyleave" },
     { label: "My Leave", icon: "pi pi-calendar-plus", url: "/myleave" },
     {
@@ -75,7 +71,7 @@ export function Navmanager({ username }) {
       : "");
 
   const items = [
-    { label: "Home", icon: "pi pi-home", url: "/DManager" },
+    { url: "/DManager" },
     { label: "Employees", icon: "pi pi-users", url: "/employees" },
     {
       label: "Leave Requests",
@@ -108,7 +104,7 @@ export function Navmanager({ username }) {
 
 export function Navadmin() {
   const items = [
-    { label: "Home", icon: "pi pi-home", url: "/Admin" },
+    { url: "/Admin" },
     { label: "Manager", icon: "pi pi-id-card", url: "/manager" },
     { label: "Department", icon: "pi pi-sitemap", url: "/department" },
     { label: "Leave Types", icon: "pi pi-calendar", url: "/leavetype" },

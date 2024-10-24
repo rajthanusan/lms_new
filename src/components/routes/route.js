6bb81e7a4e36a1a117e1539ee297dc500274eb52
rajthanusan <rajthanusan08@gmail.com>
2024-10-24 12:44:10 +0530

    loading container added

--- src/components/routes/route.js
+++ src/components/routes/route.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../common/home';
 import Login from '../common/login';
@@ -14,7 +14,13 @@ import Department from '../admin/department';
 import Whatsapp from '../admin/AdminWhatsAppPanel';
 import LeaveSummaryEmp from '../manager/leavesummaryemp';
 
-// Helper component to wrap Employees and pass the username
+
+const Loading = () => (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+       <svg xmlns="http://www.w3.org/2000/svg" width="6em" height="6em" viewBox="0 0 24 24"><g><circle cx="12" cy="2.5" r="1.5" fill="#204183" opacity="0.14"/><circle cx="16.75" cy="3.77" r="1.5" fill="#204183" opacity="0.29"/><circle cx="20.23" cy="7.25" r="1.5" fill="#204183" opacity="0.43"/><circle cx="21.5" cy="12" r="1.5" fill="#204183" opacity="0.57"/><circle cx="20.23" cy="16.75" r="1.5" fill="#204183" opacity="0.71"/><circle cx="16.75" cy="20.23" r="1.5" fill="#204183" opacity="0.86"/><circle cx="12" cy="21.5" r="1.5" fill="#204183"/><animateTransform attributeName="transform" calcMode="discrete" dur="0.75s" repeatCount="indefinite" type="rotate" values="0 12 12;30 12 12;60 12 12;90 12 12;120 12 12;150 12 12;180 12 12;210 12 12;240 12 12;270 12 12;300 12 12;330 12 12;360 12 12"/></g></svg>
+    </div>
+);
+
 const EmployeesWithProps = () => {
     const loggedInDepartmentManager = JSON.parse(sessionStorage.getItem('loggedInDepartmentManager'));
     const username = loggedInDepartmentManager ? loggedInDepartmentManager.username : '';
@@ -22,9 +28,22 @@ const EmployeesWithProps = () => {
 };
 
 export default function Routesnav() {
+    const [loading, setLoading] = useState(true);
     const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
     const loggedInAdmin = JSON.parse(sessionStorage.getItem('loggedInAdmin'));
     const loggedInDepartmentManager = JSON.parse(sessionStorage.getItem('loggedInDepartmentManager'));
+
+    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return <Loading />;
+    }
 
     return (
         <Router>
@@ -73,7 +92,7 @@ export default function Routesnav() {
                 
                 <Route
                     path="/department"
-                    element={loggedInAdmin ? <Department/> : <Navigate to="/login" />}
+                    element={loggedInAdmin ? <Department /> : <Navigate to="/login" />}
                 />
                 <Route
                     path="/leavetype"
